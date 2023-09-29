3a41d639b2731c1cabdb3a1d6b6da1d6683c5b8a
Daniel Schloms <daniel.schloms@gmail.com>
2023-09-29 17:48:22 +0200

    Add forgotten return, codelldb working

--- configs/rust-tools.lua
+++ configs/rust-tools.lua
@@ -1,8 +1,15 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
--- local capabilities = require("plugins.configs.lspconfig").capabilities
 local rt = require("rust-tools")
+local mason_registry = require("mason-registry")
+
+local codelldb = mason_registry.get_package("codelldb")
+local extension_path = codelldb:get_install_path() .. "/extension/"
+local codelldb_path = extension_path .. "adapter/codelldb"
+local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
 
 local options = {
+  dap = {
+    adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+  },
   server = {
     on_attach = function(_, bufnr)
       -- Hover actions
@@ -27,3 +34,5 @@ local options = {
     },
   }
 }
+
+return options
